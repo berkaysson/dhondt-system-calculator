c47804c9b86b1e37ec6d06162b3f9f8436bcc23c
berkaysson <berkaysonel85@gmail.com>
2023-04-18 16:08:35 +0300

    Add states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import AllocationHistory from "./Components/AllocationHistory";
 import Header from "./Components/Header";
 import InputForm from "./Components/InputForm";
 import Results from "./Components/Results";
 
 function App() {
+  const [totalSeats, setTotalSeats] = useState();
+  const [numberOfParty, setNumberOfParty] = useState(6);
+  const [parties, setParties] = useState(() => {});
+
   return (
     <div>
       <Header />
-      <InputForm />
+      <InputForm numberOfParty={numberOfParty} />
       <Results />
       <AllocationHistory />
     </div>
