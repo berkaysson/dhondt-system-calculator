9f0ed45c7b5b4a712c6cc7c6faccf95443aeb79b
berkaysson <berkaysonel85@gmail.com>
2023-04-18 16:08:13 +0300

    Add InputForm

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -1,6 +1,19 @@
-const InputForm = () => {
+import PartyForm from "./PartyForm";
+
+const InputForm = ({numberOfParty}) => {
+  const partyForms = [];
+
+  for (let i = 1; i <= numberOfParty; i++) {
+    partyForms.push(<PartyForm key={i} partyName={i} />);
+  }
+
   return <div>
-    InputForm
+    <form>
+      <label>Vekil Sayısı</label>
+      <input type="number"></input>
+      {partyForms}
+      <button>Hesapla</button>
+    </form>
   </div>
 }
 
