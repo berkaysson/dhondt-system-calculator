b5fd56906609d7675583bd6dac917f46c065318d
berkaysson <berkaysonel85@gmail.com>
2023-04-19 17:25:06 +0300

    Add code to display results.

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,7 +1,29 @@
-const Results = () => {
-  return <div>
-    Results
-  </div>
+import React from "react";
+
+function Result({results}) {
+  return (
+    <div>
+      <h2>Results</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Party</th>
+            <th>Votes</th>
+            <th>Seats</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.parties.map((party) => (
+            <tr key={party.name}>
+              <td>{party.name}</td>
+              <td>{party.votes}</td>
+              <td>{party.seats}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Results;
+export default Result;
