d5986d52b1d9fca37e0839036147723702125d3e
berkaysson <berkaysonel85@gmail.com>
2023-04-20 15:37:19 +0300

    Create Header components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,13 @@
-const Header = () => {
-  return <div>
-    HEADER
-  </div>
+function Header() {
+  return (
+    <header>
+      <h1>2023 yılı Milletvekili hesabı - D'Hondt Sistemi</h1>
+      <div id="info-container">
+        <p>Programa vekil sayısını girin ve her parti için tahmini oy sayısını girin. "Hesapla" butonuna tıklayın ve D'Hondt sistemi ile hesaplanmış sonuçları görün. "Temizle" butonuna tıklayarak formu sıfırlayabilirsiniz.</p>
+        <p>Yüzde değerleri girilebilir, ancak vekil sayısı tam sayı olacağı için 1 vekil sapması olabilir. Kesin sonuçlar için oy sayısı veya detaylı yüzde değerleri kullanılmalıdır.</p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
