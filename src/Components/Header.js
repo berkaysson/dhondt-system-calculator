aca94c5901afb19b4280f6e30dab56b5658aa2ff
berkaysson <berkaysonel85@gmail.com>
2023-04-20 16:26:44 +0300

    Style Header component using by style-components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,35 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: justify;
+  padding: var(--padding);
+
+  gap: var(--gap);
+
+  border-bottom: 1px solid var(--light-alt-color);
+`
+
+const InfoWrapper = styled.section`
+  width: 70%;
+
+  & p {
+    margin-top:1rem;
+  }
+`
+
 function Header() {
   return (
-    <header>
-      <h1>2023 yılı Milletvekili hesabı - D'Hondt Sistemi</h1>
-      <div id="info-container">
-        <p>Programa vekil sayısını girin ve her parti için tahmini oy sayısını girin. "Hesapla" butonuna tıklayın ve D'Hondt sistemi ile hesaplanmış sonuçları görün. "Temizle" butonuna tıklayarak formu sıfırlayabilirsiniz.</p>
-        <p>Yüzde değerleri girilebilir, ancak vekil sayısı tam sayı olacağı için 1 vekil sapması olabilir. Kesin sonuçlar için oy sayısı veya detaylı yüzde değerleri kullanılmalıdır.</p>
-      </div>
-    </header>
+    <HeaderWrapper>
+      <h1>2023 yılı Milletvekili hesaplayıcı - D'Hondt Sistemi</h1>
+      <InfoWrapper>
+        <p>Programa vekil sayısını girin ve her parti için tahmini oy sayısını girin. "Hesapla" butonuna tıklayın ve D'Hondt sistemi ile hesaplanmış sonuçları görün.</p>
+        <p>Yüzde değerleri girilebilir, ancak vekil sayısı tam sayı olacağı için 1 vekil sapması olabilir. Kesin sonuçlar için oy sayısı veya küsüratlı yüzde değerleri kullanılmalıdır.</p>
+      </InfoWrapper>
+    </HeaderWrapper>
   );
 }
 
