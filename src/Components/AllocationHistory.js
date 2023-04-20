e2679e65fd3a10d667ca443e7142c71e966c3223
berkaysson <berkaysonel85@gmail.com>
2023-04-20 20:01:17 +0300

    Style the AllocationHistory component

--- src/Components/AllocationHistory.js
+++ src/Components/AllocationHistory.js
@@ -1,26 +1,68 @@
 import React from "react";
+import styled from "styled-components";
+
+const AllocationHistoryWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--gap);
+  padding: var(--padding);
+`;
+
+const PartyHistory = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: var(--dark-alt-color);
+  padding: var(--padding);
+  border: 2px solid var(--light-alt-color);
+  outline: 0;
+  box-sizing: border-box;
+`;
+
+const PartyAbbreviation = styled.h3`
+  margin-bottom: var(--margin);
+`;
+
+const AllocationList = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+const AllocationItem = styled.li`
+  margin-bottom: var(--margin);
+`;
+
+const SeatNumber = styled.span`
+  font-weight: bold;
+`;
+
+const AllocationPercentage = styled.span`
+  margin-left: var(--margin);
+`;
 
 function AllocationHistory({ parties }) {
   const allocationHistoryContent = parties.map((party, index) => {
     if (party.allocationHistory.length !== 0) {
       return (
-        <div key={index}>
-          <h3>{party.abb}</h3>
-          <ul>
+        <PartyHistory key={index}>
+          <PartyAbbreviation>{party.abb}</PartyAbbreviation>
+          <AllocationList>
             {party.allocationHistory.map((allocation, index) => (
-              <li key={index}>
-                Seat {allocation.seat} ({allocation.percentage}%)
-              </li>
+              <AllocationItem key={index}>
+                Seat <SeatNumber>{allocation.seat}</SeatNumber>
+                <AllocationPercentage>
+                  ({allocation.percentage})
+                </AllocationPercentage>
+              </AllocationItem>
             ))}
-          </ul>
-        </div>
+          </AllocationList>
+        </PartyHistory>
       );
     }
 
     return null;
   });
 
-  return <div>{allocationHistoryContent}</div>;
+  return <AllocationHistoryWrapper>{allocationHistoryContent}</AllocationHistoryWrapper>;
 }
 
 export default AllocationHistory;
