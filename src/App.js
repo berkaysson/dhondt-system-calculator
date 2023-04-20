8dbccf1e3e53d0769467477118fa2ad4bf1c8383
berkaysson <berkaysonel85@gmail.com>
2023-04-20 15:44:40 +0300

    Extract PARTIES to Data folder and create partiesData.js

--- src/App.js
+++ src/App.js
@@ -1,103 +1,12 @@
 import { useState } from "react";
+
 import Header from "./Components/Header";
 import InputForm from "./Components/InputForm";
 import Results from "./Components/Results";
 
-const PARTIES = [
-  {
-    id: 1,
-    name: "Adalet ve Kalkınma Partisi",
-    ittifak: "Cumhur İttifakı",
-    abb: "AKP",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 2,
-    name: "Cumhuriyet Halk Partisi",
-    ittifak: "Millet İttifakı",
-    abb: "CHP",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 3,
-    name: "Yeşiller ve Sol Gelecek Partisi",
-    ittifak: "Emek ve Özgürlük İttifakı",
-    abb: "YSGP(HDP)",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 4,
-    name: "İYİ Parti",
-    ittifak: "Millet İttifakı",
-    abb: "İYİ",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 5,
-    name: "Milliyetçi Hareket Partisi",
-    ittifak: "Cumhur İttifakı",
-    abb: "MHP",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 6,
-    name: "Memleket Partisi",
-    ittifak: "-",
-    abb: "MP",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 7,
-    name: "Yeniden Refah Partisi",
-    ittifak: "Cumhur İttifakı",
-    abb: "YRP",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 8,
-    name: "Türkiye İşçi Partisi",
-    ittifak: "Emek ve Özgürlük İttifakı",
-    abb: "TİP",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 9,
-    name: "Zafer Partisi",
-    ittifak: "Ata İttifakı",
-    abb: "ZAFER",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-  {
-    id: 10,
-    name: "Herhangi Bir Parti",
-    ittifak: "-",
-    abb: "Diğer, bağımsız",
-    votes: 0,
-    seats: 0,
-    allocationHistory: [],
-  },
-];
+import { PARTIES } from "./Data/partiesData";
 
 const NUMBER_OF_PARTIES = 10;
-
 
 function App() {
   const [electionData, setElectionData] = useState({
@@ -172,7 +81,7 @@ function App() {
         parties = {electionData.parties}
       />
       <Results results={electionData} />
-    </div>
+    </AppWrapper>
   );
 }
 
