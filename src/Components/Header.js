9780d0c7c7e276d681588740007d8afcef525b33
berkaysson <berkaysonel85@gmail.com>
2023-04-22 23:35:48 +0300

    Add warning

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,6 +18,10 @@ const HeaderWrapper = styled.header`
   border-bottom: 1px solid var(--light-alt-color);
 `
 
+const WarningMessage = styled.p`
+  color: var(--theme-color);
+`
+
 const InfoWrapper = styled.section`
   width: 80%;
   & p {
@@ -31,7 +35,8 @@ function Header() {
       <InfoWrapper>
       <h1>2023 yılı Milletvekili hesaplayıcı - D'Hondt Sistemi</h1>
         <p>Programa vekil sayısını girin ve her parti için tahmini oy sayısını girin. "Hesapla" butonuna tıklayın ve D'Hondt sistemi ile hesaplanmış sonuçları görün.</p>
-        <p>Yüzde değerleri girilebilir, ancak vekil sayısı tam sayı olacağı için 1 vekil sapması olabilir. Kesin sonuçlar için oy sayısı veya küsüratlı yüzde değerleri kullanılmalıdır.</p>
+        <p>Yüzde değerleri girilebilir, ancak vekil sayısı tam sayı olacağı için 1 vekil sapması olabilir (Eşitlik durumunda). Kesin sonuçlar için oy sayısı veya küsüratlı yüzde değerleri kullanılmalıdır.</p>
+        <WarningMessage>Bölgeyi seç butonuna basmazsanız seçili bölgeyi hesaba katmaz.</WarningMessage>
       </InfoWrapper>
     </HeaderWrapper>
   );
